6e3713bb07f73e0c86314d607e0ab609c9026620
Sandrine14000 <108656015+Sandrine14000@users.noreply.github.com>
2022-07-19 15:12:27 +0200

    up

--- exo2/exo2.js
+++ exo2/exo2.js
@@ -1,8 +1,10 @@
 let x = prompt("Quelle est la valeur de x ?");
 let y = prompt("Quelle est la valeur de y ?");
+
 if (x > y){
     p1= "x est plus grand que y";
 }
-else if (y> x){
-    p1="x est plus grand que y";
+else if (x <= y){
+    p1="y est plus grand que x";
 }
+alert(p1);
