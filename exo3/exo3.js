1adb221b6c7898bba161bbd73b05d378507606d9
Sandrine14000 <108656015+Sandrine14000@users.noreply.github.com>
2022-07-19 16:18:54 +0200

    up exo 3

--- exo3/exo3.js
+++ exo3/exo3.js
@@ -2,3 +2,11 @@ for (let ligne = 0; ligne <= 10; ligne++) {
     console.log ("ligne n° "+ligne)
     
 }
+
+let nbLigne = prompt("Quel est le nombre de Lignes ?");
+alert(nbLigne);
+
+if (nbLigne >= 100){
+    alert ("On ne peux utiliser plus de 100 lignes");
+}
+alert("Le nombre de Lignes est : "+nbLigne);
