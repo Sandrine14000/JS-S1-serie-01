07fbe64e170c7a721e658dbd0e2968d53fb6f024
Sandrine14000 <108656015+Sandrine14000@users.noreply.github.com>
2022-07-21 14:32:12 +0200

    fin exo 11

---
+++ Exo-11/exo-11.js
@@ -0,0 +1,9 @@
+let reponse = prompt("De quelle couleur est le soleil?");
+
+let repPara =document.getElementById("p1");
+if (reponse=="jaune"){
+    repPara.innerHTML = "C'est la bonne réponse";
+}
+else if(reponse!="jaune") {
+    repPara.innerHTML = "Mauvaise réponse !";
+}
