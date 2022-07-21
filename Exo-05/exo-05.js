7d42a34acb5f36ee46f2a152fb5055d659c5a88d
Sandrine14000 <108656015+Sandrine14000@users.noreply.github.com>
2022-07-21 10:40:01 +0200

    fin exo 5

---
+++ Exo-05/exo-05.js
@@ -0,0 +1,8 @@
+let rayon = 10;
+
+//A = π x r²
+rayoncarre = Math.pow(rayon, 2);
+let surface = ((Math.PI).toFixed(3))* rayoncarre;
+
+console.log("le cercle de rayon "+ rayon + " cm pour surface "+ surface+ 
+" cm2");
