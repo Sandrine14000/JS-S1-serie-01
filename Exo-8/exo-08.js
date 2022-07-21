57cdbb79d27de675f3de09c5153268ed483ef31c
Sandrine14000 <108656015+Sandrine14000@users.noreply.github.com>
2022-07-21 11:40:56 +0200

    fin exo 8

---
+++ Exo-8/exo-08.js
@@ -0,0 +1,6 @@
+let longueur = prompt("Quel est la longueur du rectangle ?");
+let largeur = prompt("Quel est la largeur du rectangle ?");
+
+let surface = longueur*largeur;
+
+console.log ("le rectangle de longueur "+longueur+" cm et de largeur "+largeur+" cm a pour surface "+surface+ " cm2");
