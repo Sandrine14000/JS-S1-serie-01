b51ef33c6925abb3188133d3ff58d209e0bfd360
Sandrine14000 <108656015+Sandrine14000@users.noreply.github.com>
2022-07-21 10:03:10 +0200

    fin exo 3

---
+++ Exo-3/exo-03.js
@@ -0,0 +1,4 @@
+let temperatureCelsius = 0;
+let temperatureKelvin = temperatureCelsius + 273.15;
+
+console.log(`La température de ${temperatureCelsius}°Celsius vaut ${temperatureKelvin} en Kelvin`); 
