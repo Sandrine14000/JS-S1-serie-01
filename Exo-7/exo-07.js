537b5bf015c466e0dce5fc87a878799afa424ac8
Sandrine14000 <108656015+Sandrine14000@users.noreply.github.com>
2022-07-21 11:34:06 +0200

    fin exo 7

---
+++ Exo-7/exo-07.js
@@ -0,0 +1,6 @@
+let prenom = prompt("Quel est votre prenom ?");
+let age = parseInt (prompt("Quel est votre age ?"));
+
+nvAge= age+1;
+
+alert (`je m'appelle ${prenom} et j'ai ${nvAge} ans l'année prochaine`);
