05297bedf0c6ef72b58eebe0a4c4dbaaa5f8d65e
Sandrine14000 <108656015+Sandrine14000@users.noreply.github.com>
2022-07-21 11:51:33 +0200

    fin exo 9

---
+++ Exo-9/exo-09.js
@@ -0,0 +1,5 @@
+let tempCelsius = parseInt(prompt ("Quelle est la temperature en degrés Celsius ?"));
+let tempKelvin = tempCelsius+ 273.15;
+
+
+console.log(" La température de "+tempCelsius+" °Celsius vaut "+tempKelvin+" en Kelvin");
